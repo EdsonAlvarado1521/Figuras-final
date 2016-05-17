173b36b8cf8ef7b20e06033f5e1cbe6286de900c
Edson Alvarado <edson-0059@hotmail.com>
2016-05-16 22:11:02 -0700

    Proyecto figuras
    El proyecto de figuras incluye:
    -Cambio de color de las figuras
    -Dibuja rectangulos, circulos, rectas y poligonos
    -Ordena las figuras de menor a mayor
    -Borra las figuras

---
+++ WindowsFormsApplication2/Figura.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication2
+{
+    abstract class Figura:IComparable
+    {
+        protected int X;
+        protected int Y;
+       
+        protected int R1, R2, R3, R4;     //coordenadas de las rectas
+        protected int P1, P2, P3, P4, P5;  //coordenadas para un poligono
+        protected Pen pluma;
+        protected int ancho;
+        protected int largo;
+        protected Color color1,color2;
+        protected SolidBrush brocha;
+
+        public Figura(int x, int y, Color c1, Color c2 ) {
+            X = x;
+            Y = y;
+            color1 = c1;
+            color2 = c2;
+            brocha = new SolidBrush(color1);
+            pluma = new Pen(color2, 4);
+                
+            Random rnd = new Random();
+            ancho = rnd.Next(10,60);
+            largo = ancho;
+            R1 = rnd.Next(20, 300);
+            R2 = rnd.Next(20, 300);
+            R3 = rnd.Next(20, 300);
+            R4 = rnd.Next(20, 300);
+            P1 = rnd.Next(20, 500);
+            P2 = rnd.Next(20, 500);
+            P3 = rnd.Next(20, 500);
+            P4 = rnd.Next(20, 500);
+            P5 = rnd.Next(20, 500);
+        }
+
+        public abstract void Draw(Form f);
+
+        public int CompareTo(object obj)
+        {
+
+            return this.largo.CompareTo(((Figura)obj).largo);
+        }
+    }
+
+
+    class Rectangulo:Figura
+    {
+        public Rectangulo(int x, int y, Color c1, Color c2 ):base(x,y, c1, c2)
+    	{
+        } 
+
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawRectangle(pluma, this.X, this.Y, ancho, largo);
+            g.FillRectangle(brocha, this.X, this.Y, ancho, largo);
+        }
+
+    }
+
+    class Circulo : Figura
+    {
+        public Circulo(int x, int y, Color c1, Color c2) : base(x, y, c1, c2)
+        {
+
+        }
+
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawEllipse(pluma, this.X, this.Y, ancho, largo);
+            g.FillEllipse(brocha, this.X, this.Y, ancho, largo);
+        }
+
+    }
+
+    class Recta  : Figura
+    {
+        public Recta(int x, int y, Color c1, Color c2) : base(x, y, c1, c2)
+        {
+
+        }
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+
+            Point coordenada1 = new Point(this.R1, this.R2);
+            Point coordenada2 = new Point(this.R3, this.R4);
+
+            GraphicsPath linea = new GraphicsPath();
+            linea.AddLine(coordenada1, coordenada2);
+            g.DrawPath(pluma, linea);
+        }
+    }
+
+    class Poligono : Figura
+    {
+        public Poligono(int x, int y, Color c1, Color c2) : base(x, y, c1, c2)
+        {
+
+        }
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+
+            Point punto1 = new Point(this.P1, 100);
+            Point punto2 = new Point(this.P2, 50);
+            Point punto3 = new Point(this.P3, 200);
+            Point punto4 = new Point(this.P4, 350);
+            Point punto5 = new Point(this.P5, 300);
+            GraphicsPath trazar = new GraphicsPath();
+            trazar.AddLine(punto1, punto2);
+            trazar.AddLine(punto2, punto3);
+            trazar.AddLine(punto3, punto4);
+            trazar.AddLine(punto4, punto5);
+            trazar.AddLine(punto5, punto1);
+            g.DrawPath(pluma, trazar);
+            g.FillPath(brocha, trazar);
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+
+    public partial class Form1 : Form
+    {
+        enum TipoFigura  {Rectangulo, Circulo, Recta, Poligono};
+        private Color relleno, contorno;
+        private TipoFigura figura_actual; 
+        private List<Figura> rectangulos;
+        
+
+        public Form1()
+        {
+            figura_actual = TipoFigura.Circulo;
+            relleno = Color.AliceBlue;
+            contorno = Color.Black;
+            rectangulos = new List<Figura>();
+            InitializeComponent();
+           
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (figura_actual == TipoFigura.Circulo)
+            {
+                Circulo c = new Circulo(e.X, e.Y, relleno, contorno);
+                c.Draw(this);
+                rectangulos.Add(c);
+            }
+            else if (figura_actual == TipoFigura.Rectangulo)
+            {
+                Rectangulo r = new Rectangulo(e.X, e.Y, relleno, contorno);
+                r.Draw(this);
+                rectangulos.Add(r);
+            }
+            else if (figura_actual == TipoFigura.Recta)
+            {
+                Recta rt = new Recta(e.X, e.Y, relleno, contorno);
+                rt.Draw(this);
+                rectangulos.Add(rt);
+            }
+            else if (figura_actual == TipoFigura.Poligono)
+            {
+                Poligono p = new Poligono(e.X, e.Y, relleno, contorno);
+                p.Draw(this);
+                rectangulos.Add(p);
+            }
+            
+          
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Polimorfismo
+            foreach (Figura r in rectangulos)
+                r.Draw(this);
+        }
+
+        
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        
+
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+            figura_actual = TipoFigura.Circulo;
+        }
+
+        private void toolStripButton2_Click_1(object sender, EventArgs e)
+        {
+            figura_actual = TipoFigura.Rectangulo;
+        }
+
+        private void toolStripButton3_Click_1(object sender, EventArgs e)
+        {
+            figura_actual = TipoFigura.Recta;
+        }
+
+        private void toolStripButton4_Click_1(object sender, EventArgs e)
+        {
+            figura_actual = TipoFigura.Poligono;
+        }
+
+        private void toolStripButton5_Click_1(object sender, EventArgs e)
+        {
+            rectangulos.Clear();
+            this.Refresh();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            rectangulos.Sort();
+            rectangulos.Reverse();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            ColorDialog color1 = new ColorDialog();
+            color1.ShowDialog();
+            relleno = color1.Color;
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+            ColorDialog color2 = new ColorDialog();
+            color2.ShowDialog();
+            contorno = color2.Color;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
